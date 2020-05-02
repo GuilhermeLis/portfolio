842c324389a335a4705f924600cb91838b989ce4
Guilherme Lisboa <guilisboadasilva@gmail.com>
2020-05-02 17:58:48 -0300

    start react-app js-template

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function home() {
+export default function Home() {
   return (
     <Container>
       <div className = "info">
---
+++ src/router.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+
+import Home from './pages/Home'
+import { history }  from '../src/services/history'
+
+export default function pages() {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+}
